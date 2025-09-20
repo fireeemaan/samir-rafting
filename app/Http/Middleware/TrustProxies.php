be77c5a2bbd8d69d52bbd98aad8395542bff1b5a
fireeemaan <fireeemaan222@gmail.com>
2025-09-20 09:19:20 +0700

    Update TrustProxies middleware to include additional headers for proxy handling

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -15,7 +15,12 @@ class TrustProxies
      */
 
     protected $proxies = '*';
-    protected $headers = Request::HEADER_X_FORWARDED_FOR;
+    protected $headers = Request::HEADER_X_FORWARDED_FOR |
+                        Request::HEADER_X_FORWARDED_HOST |
+                        Request::HEADER_X_FORWARDED_PORT |
+                        Request::HEADER_X_FORWARDED_PROTO |
+                        Request::HEADER_X_FORWARDED_PREFIX |
+                        Request::HEADER_X_FORWARDED_TRAEFIK;
 
     public function handle(Request $request, Closure $next): Response
     {
